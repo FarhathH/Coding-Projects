75a06a870ca977efecd7e9bce89fc8335245c5ef
FarhathH <farhathh0@gmail.com>
2024-05-28 13:19:21 +0100

    Re-doing the coding structure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 //  A convenient folder where I have begun some projects to do in my spare time.
 //  Sweets project:
     - A vending machine app for depositing chocolates and gummies.
-    -
+    - Date period : Feb 2024 - present
--- sweets/treats/treats.cpp
+++ sweets/treats/treats.cpp
@@ -2,65 +2,13 @@
 #include <string>
 using namespace std;
 
-/*
-    This trying to redo the vending machine concept again as my 
-    file has gone either corrupted or faulty.
-*/
-class Treats //main class and containing the bare minimum essentials.
-{    
-    public:
-        
-        Treats()
-        {
-            cout << "******************************************" << endl;
-        }
 
-        void viewTreats();
-        void selectTreats();
-};
 
-class Gummies : Treats
-{
-
-    void selectTreat()
-    {
-        
-    }
-    
-};
-
-class Chocolate : Treats
-{
-    public:
-        string chocoList[5] = {"Mars Bars", "KitKat", "Bounty", "Snickers", "Ferrero Roche"};
-        double chocoPrices[5] = {0.10, 0.57, 0.45, 0.39, 0.21};
-        int treat;
-
-        void viewTreats()
-        {
-            cout << "Pick any treat you want by pressing the corresponding" << endl;
-            cout << chocoList << endl;
-        }
-
-        void selectTreat()
-        {
-
-        }
-        
-};
 
 int main()
 {
-    
-   cout << "Hello World" << endl;
-
-   Chocolate c1;
-   Gummies c12;
-
-   c1.viewTreats();
-
-   
-    
+    cout << "************" << endl;
+    cout << "Hello World" << endl;
     
     return 0;
 }
