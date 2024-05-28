3aaacb28007e3f5fdb85801b3ecf5901737b5c78
FarhathH <farhathh0@gmail.com>
2024-05-28 17:54:12 +0100

    Just some changes.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 //  A convenient folder where I have begun some projects to do in my spare time.
 //  Sweets project:
-    - A vending machine app for depositing chocolates and gummies.
-    - Date period : Feb 2024 - present
+    - A vending machine app for depositing snacks.
+    - Date period : Feb 2024 - present.
+    - I wanted to test out my current coding approach when writing a program.
+    - Went with basic OOP and other coding concepts I had learned a while ago in Uni.
+    - To prove to myself that I am able to put the knowledge into practice.
--- sweets/treats/treats.cpp
+++ sweets/treats/treats.cpp
@@ -2,13 +2,51 @@
 #include <string>
 using namespace std;
 
+class Sweets
+{
+    public:
 
+        string snacks[2][5] = { //multidimentional array to display snacks and prices.
+            {"Marsbars","KitKat", "Bounty", "Galaxy", "Raffelo"},
+            {"0.50", "0.65", "0.70", "0.23", "0.19"}
+            };
+
+        double prices[5] = {0.50, 0.65, 0.70, 0.23, 0.19};
+        double credits = 4.67;
+
+        Sweets()
+        {
+            cout << "************" << endl; //This is just to let me know that I can instantiate an object with no errors. 
+        }
+
+        void viewList()
+        {
+            cout << "Menu:" << endl;
+            
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    cout << snacks[i][j] << endl;
+                }
+            }
+        }
+
+        void viewCredits()
+        {
+            cout << "************" << endl;
+            cout << "Credits: " << credits << endl;
+        }
+};
 
 
 int main()
 {
-    cout << "************" << endl;
-    cout << "Hello World" << endl;
+
+    Sweets p;
+    p.viewList();
+    p.viewCredits();
+
     
     return 0;
 }
