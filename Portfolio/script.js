d5b3752f1c783b02bdf750d0140248a803ac1b49
FarhathH <farhathh0@gmail.com>
2024-07-28 01:10:55 +0100

    Starting to get the hang of JavaScript HTML DOM

---
+++ Portfolio/script.js
@@ -0,0 +1,59 @@
+function home(){
+    document.body.style.backgroundColor = "blue";
+}
+
+function homeHover(){
+    document.getElementById("home").style.backgroundColor = "blue";
+    document.getElementById("home").style.color = "lightblue";
+}
+
+function homeExit(){
+    document.getElementById("home").style.backgroundColor = "beige";
+    document.getElementById("home").style.color = "black";
+}
+
+function about(){
+    document.body.style.backgroundColor = "#6A89A7";
+}
+
+function aboutHover(){
+    document.getElementById("about").style.backgroundColor = "#6A89A7";
+    document.getElementById("about").style.color = "#B7C9E2";
+}
+
+function aboutExit(){
+    document.getElementById("about").style.backgroundColor = "beige";
+    document.getElementById("about").style.color = "black";
+}
+
+function digitalArt(){
+    document.body.style.backgroundColor = "#FF69B4";
+}
+
+function digitalArtHover(){
+    document.getElementById("digitalArt").style.backgroundColor = "#FF69B4";
+    document.getElementById("digitalArt").style.color = "#FFB3DE";
+}
+
+function digitalArtExit(){
+    document.getElementById("digitalArt").style.backgroundColor = "beige";
+    document.getElementById("digitalArt").style.color = "black";
+}
+
+function animation(){
+    document.body.style.backgroundColor = "#0D050D";
+    
+}
+
+function animationHover(){
+    document.getElementById("animation").style.backgroundColor = "#0D050D";
+    document.getElementById("animation").style.color = "#F2EBF3";
+}
+
+function animationExit(){
+    document.getElementById("animation").style.backgroundColor = "beige";
+    document.getElementById("animation").style.color = "black";
+}
+
+
+
