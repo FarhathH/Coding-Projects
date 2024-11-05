ff88c6aff065e34b3fbbe1f57545e83cfbc28f52
FarhathH <farhathh0@gmail.com>
2024-11-05 00:45:38 +0000

    Just attempting to add a calculator on the webpage.

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -10,21 +10,27 @@ function home(){
     document.getElementsByClassName("space")[3].style.color = "lightblue";
     document.getElementsByClassName("space")[4].style.backgroundColor = "blue";
     document.getElementsByClassName("space")[4].style.color = "lightblue";
-    
+    document.getElementsByClassName("space")[5].style.backgroundColor = "blue";
+    document.getElementsByClassName("space")[5].style.color = "lightblue";
+    document.getElementById("hurry").innerHTML = "◑____________◑";
 
     document.getElementsByClassName("space")[0].innerHTML = "Home sweet home.";
-    document.getElementsByClassName("space")[1].innerHTML = 
-    document.getElementsByClassName("space")[0].style.flex = "0.01";
     document.getElementsByClassName("space")[0].style.transition = "2s";
+    document.getElementsByClassName("space")[1].style.transition = "2s";
+    document.getElementsByClassName("space")[2].style.transition = "2s";
+    document.getElementsByClassName("space")[3].style.transition = "2s";
+    document.getElementsByClassName("space")[4].style.transition = "2s";
+    document.getElementsByClassName("space")[5].style.transition = "2s";
+    document.getElementById("hurry").style.transition = "2s";
 }
 
-function homeHover(){
+function homeHover(){ //function for mouse hover
     document.getElementById("home").style.backgroundColor = "blue";
     document.getElementById("home").style.color = "lightblue";
   
 }
 
-function homeExit(){
+function homeExit(){ //function for mouse moving off
     document.getElementById("home").style.backgroundColor = "beige";
     document.getElementById("home").style.color = "black";
 }
@@ -41,9 +47,18 @@ function about(){
     document.getElementsByClassName("space")[3].style.color = "#B7C9E2";
     document.getElementsByClassName("space")[4].style.backgroundColor = "#6A89A7";
     document.getElementsByClassName("space")[4].style.color = "#B7C9E2";
+    document.getElementsByClassName("space")[5].style.backgroundColor = "#6A89A7";
+    document.getElementsByClassName("space")[5].style.color = "#B7C9E2";
+    document.getElementById("hurry").innerHTML = "( ͒꒪̛ཅ꒪̛ ͒)";
 
     document.getElementsByClassName("space")[0].innerHTML = "This is a webpage to display my work";
     document.getElementsByClassName("space")[0].style.transition = "2s";
+    document.getElementsByClassName("space")[1].style.transition = "2s";
+    document.getElementsByClassName("space")[2].style.transition = "2s";
+    document.getElementsByClassName("space")[3].style.transition = "2s";
+    document.getElementsByClassName("space")[4].style.transition = "2s";
+    document.getElementsByClassName("space")[5].style.transition = "2s";
+    document.getElementById("hurry").style.transition = "2s";
 }
 
 function aboutHover(){
@@ -68,9 +83,18 @@ function digitalArt(){
     document.getElementsByClassName("space")[3].style.color = "#FFB3DE";
     document.getElementsByClassName("space")[4].style.backgroundColor = "#FF69B4";
     document.getElementsByClassName("space")[4].style.color = "#FFB3DE";
+    document.getElementsByClassName("space")[5].style.backgroundColor = "#FF69B4";
+    document.getElementsByClassName("space")[5].style.color = "#FFB3DE";
+    document.getElementById("hurry").innerHTML = "(o・┏ω┓・o)";
 
     document.getElementsByClassName("space")[0].innerHTML = "Just my digital art";
     document.getElementsByClassName("space")[0].style.transition = "2s";
+    document.getElementsByClassName("space")[1].style.transition = "2s";
+    document.getElementsByClassName("space")[2].style.transition = "2s";
+    document.getElementsByClassName("space")[3].style.transition = "2s";
+    document.getElementsByClassName("space")[4].style.transition = "2s";
+    document.getElementsByClassName("space")[5].style.transition = "2s";
+    document.getElementById("hurry").style.transition = "2s";
 }
 
 function digitalArtHover(){
@@ -95,9 +119,18 @@ function animation(){
     document.getElementsByClassName("space")[3].style.color = "#F2EBF3";
     document.getElementsByClassName("space")[4].style.backgroundColor = "#0D050D";
     document.getElementsByClassName("space")[4].style.color = "#F2EBF3";
+    document.getElementsByClassName("space")[5].style.backgroundColor = "#0D050D";
+    document.getElementsByClassName("space")[5].style.color = "#F2EBF3";
+    document.getElementById("hurry").innerHTML = "ƪ( ` ▿▿▿▿ ´ ƪ) ";
 
     document.getElementsByClassName("space")[0].innerHTML = "Time to animate";
     document.getElementsByClassName("space")[0].style.transition = "2s";
+    document.getElementsByClassName("space")[1].style.transition = "2s";
+    document.getElementsByClassName("space")[2].style.transition = "2s";
+    document.getElementsByClassName("space")[3].style.transition = "2s";
+    document.getElementsByClassName("space")[4].style.transition = "2s";
+    document.getElementsByClassName("space")[5].style.transition = "2s";
+    document.getElementById("hurry").style.transition = "2s";
 }
 
 function animationHover(){
@@ -112,6 +145,11 @@ function animationExit(){
 
 function paraHover(){
     document.getElementById("hurry").style.transform.rotate = 90;
+}
+
+function addPlease(num1, num2){
+    let answer = num1 + num2;
+    document.getElementById("").innerHTML
 }
 
 
