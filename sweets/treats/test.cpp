a96ce45f06b0002c45ee3391c1273b1f87c43d1b
FarhathH <farhathh0@gmail.com>
2025-02-06 11:42:48 +0000

    testing out code

--- sweets/treats/test.cpp
+++ sweets/treats/test.cpp
@@ -1,3 +1,4 @@
+//testing out anything I want to add onto my main file.
 using namespace std;
 #include <string>
 #include <iostream>
@@ -7,7 +8,18 @@ int main()
     cout << "Hello world" << endl;
 
     string Choco[2][3] = 
-    {"Mars", "Milkyway", ""},
-    {};
+    { 
+    {"Mars", "Milkyway", "Lion"},
+    {"0.60", "0.45", "1.83"}
+    };
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            cout << Choco[i][j] << endl;
+        }
+    }
+    
     return 0;
 }
