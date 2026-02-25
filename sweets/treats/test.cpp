8a78befaf30671043133ff7c457d48e0107433a6
FarhathH <farhathh0@gmail.com>
2026-02-25 05:56:58 +0000

    trying to solve a coding problem

--- sweets/treats/test.cpp
+++ sweets/treats/test.cpp
@@ -4,34 +4,17 @@
 #include <windows.h>
 using namespace std;
 
-/*
-	void pushX(int num){
-		for(int i=0;i<num;i++)
-			cout<<" ";
-	}
-	int main() {
-		for(int j=0;j<50;j++){
-			system("CLS");
-			pushX(j);
-			cout<<"Hello";
-			Sleep(100);
-		}
-
-	}
-*/
 int main()
 {
-	string snacks[2][5] = { //multidimentional array to display snacks and prices.
-		{"Marsbars","KitKat", "Bounty", "Galaxy", "Raffelo"},
-		{"0.50", "0.65", "0.70", "0.23", "0.19"}
-		};
-	
-	for(int i = 0; i < 2; i++)
-	{
-		for(int j = 0; j < 5; j++)
-		{
-			cout << snacks[i][j] << endl;
-		}
-	}
+	int n;
+    cin >> n; //size of the array
+    //your code goes here
+    int * nums = new int[n];
 
+    int *max = &nums;
+    for(int i=0; i<n; i++) {
+        if(nums[i]<max)
+            max += nums[i];
+    }
+    cout << max << endl;
 }
