7cf59c8ae04b2234b8f735f2b830240d3311c151
FarhathH <farhathh0@gmail.com>
2026-02-27 15:36:23 +0000

    Trying to find solutions through debugging

--- sweets/treats/test.cpp
+++ sweets/treats/test.cpp
@@ -1,20 +1,23 @@
 //testing out anything I want to add onto my main file.
 #include <iostream>
 #include <string>
-#include <windows.h>
 using namespace std;
 
 int main()
 {
 	int n;
     cin >> n; //size of the array
+    int size = n;
     //your code goes here
-    int * nums = new int[n];
+    int *nums = new int[size];
 
-    int *max = &nums;
-    for(int i=0; i<n; i++) {
+    
+    int max;
+    for(int i=0; i<size; i++) {
         if(nums[i]<max)
-            max += nums[i];
+            max = nums[i];
     }
     cout << max << endl;
+    
+    return 0;
 }
